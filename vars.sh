4ed4581c4f3cb21060a1ed1e2b50f9712d303512
root <root@virt03-234.pok.stglabs.ibm.com>
2014-06-13 02:18:57 -0400

    publichost

--- vars.sh
+++ vars.sh
@@ -4,4 +4,4 @@ export GIT_NAME='MyVendor Jenkins'
 export UPSTREAM_GERRIT_USER=jaypipes-testing
 export UPSTREAM_GERRIT_SSH_KEY_PATH=gerrit_key
 export JENKINS_SSH_KEY_PATH=jenkins_key
-export PUBLISH_HOST=127.0.0.1
+export PUBLISH_HOST=9.114.34.238
